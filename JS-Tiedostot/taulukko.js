9bfa1050ebe6ce6887496ade7cdead7ebe65edb8
Markus Kesseli <markus.kesseli@gmail.com>
2022-09-14 11:28:39 +0300

    java

---
+++ JS-Tiedostot/taulukko.js
@@ -0,0 +1,17 @@
+function esimerkkiTaulukko()
+
+{
+  var autot = [];
+  var caarat = ["Tesla", " VW i3", " Hyundai Ionic"];
+  autot.push("jeep");
+  document.write("Caara[1] = " + caarat[1] + " / autot[0] = " + autot[0] + "<br />");
+  document.write("Caara = " + caarat + "<br />");
+  document.write("Autot = " + autot + "<br />");
+  var siirrettavat = caarat.pop();
+  document.write("Siirrettävät = " + siirrettavat);
+  document.write("<br />Caarat taulukko = " + caarat);
+  autot.unshift(siirrettavat);
+  document.write("<br />Autot Taulukko = " + autot);
+  //caarat.push() = autot.pop(); Selain ei tykkää tästä
+  document.write("<br /> Caarat taulukko = " + caarat + "<br /> Autot Taulukko = " + autot);
+}
