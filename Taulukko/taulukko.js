6594552e3d005d20f4e7f1d0ab9afb56c4d371c2
Markus Kesseli <markus.kesseli@gmail.com>
2022-12-07 09:11:24 +0200

    Harjoituksia

---
+++ Taulukko/taulukko.js
@@ -0,0 +1,23 @@
+function taulukko()
+
+  {
+    var taulukko =
+  [
+    [
+        'Nimi',
+      [
+          ['Markus', 'Kesseli'], ['Maria', 'Kesseli']
+      ],
+        'Lähiosoite',
+      [
+          ['Ropakkotie', 8]
+      ],
+        'Postinumero',
+      [
+          ['01800', 'Klaukkala']
+      ]
+    ]
+  ];
+
+    console.log(taulukko[0][1][0][0])
+  }
